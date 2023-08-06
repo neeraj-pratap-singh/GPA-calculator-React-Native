ee74578ee9e61f0bbd5f87a3a542e5ae5ff18271
neeraj-singh <71137350+neeraj-pratap-singh@users.noreply.github.com>
2023-08-06 12:55:57 +0530

    updated text input component

--- src/components/RowComponent/RowComponent.tsx
+++ src/components/RowComponent/RowComponent.tsx
@@ -1,45 +1,39 @@
+// TextInputComponent.tsx
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
-import { TypeDropDown } from '../../components';
-import { TextInputComponent } from '../../components';
-import { GradeDropDown } from '../../components';
+import { TextInput, StyleSheet, Dimensions } from 'react-native';
 
-interface RowComponentProps {
-  // specify the props you want to pass down to the child components
-  onTypeChange: (type: string) => void;
-  onTextInputChange: (text: string) => void;
-  onGradeChange: (grade: string) => void;
-  typeValue: string;
-  textInputValue: string;
-  gradeValue: string;
+interface TextInputComponentProps {
+  value?: string;
+  onChange?: (text: string) => void;
+  style?: {};
 }
 
-const RowComponent: React.FC<RowComponentProps> = ({
-  onTypeChange,
-  onTextInputChange,
-  onGradeChange,
-  textInputValue,
+const TextInputComponent: React.FC<TextInputComponentProps> = ({
+  value,
+  onChange,
+  style,
 }) => {
   return (
-    <View style={styles.container}>
-      <TypeDropDown onTypeChange={onTypeChange} />
-      <TextInputComponent
-        value={textInputValue}
-        onValueChange={onTextInputChange}
-        error={''}
-      />
-      <GradeDropDown onGradeChange={onGradeChange} />
-    </View>
+    <TextInput
+      value={value}
+      onChangeText={onChange}
+      placeholder="Enter course"
+      placeholderTextColor="#888" // Making placeholder more visible
+      style={[styles.input, style]}
+    />
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    marginVertical: 10,
+  input: {
+    width: Dimensions.get('window').width * (1 / 2.8),
+    backgroundColor: 'white',
+    color: 'black',
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    borderBottomWidth: 1,
+    borderColor: '#555', // Darkened bottom border color
   },
 });
 
-export default RowComponent;
+export default TextInputComponent;
--- src/components/TypeDropDown/TypeDropDown.tsx
+++ src/components/TypeDropDown/TypeDropDown.tsx
@@ -1,30 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import DropDownPicker from 'react-native-dropdown-picker';
+import { Dimensions } from 'react-native';
 import { useTranslation } from 'react-i18next';
 import { useTheme } from '../../hooks';
 
 interface TypeDropDownProps {
-  onTypeChange: (item: string) => void;
+  onTypeChange?: (item: string) => void;
+  defaultValue?: string;
 }
 
-const TypeDropDown: React.FC<TypeDropDownProps> = ({ onTypeChange }) => {
+const TypeDropDown: React.FC<TypeDropDownProps> = ({ onTypeChange, defaultValue }) => {
   const { t } = useTranslation(['example', 'welcome']);
   const { darkMode: isDark } = useTheme();
+
+  // Internal state to manage value for uncontrolled usage
+  const [internalValue, setInternalValue] = useState<string | null>(defaultValue || null);
 
   const items = ['AP', 'Honours', 'Regular'].map(type => ({
     label: type,
     value: type,
   }));
 
+  const width = Dimensions.get('window').width / 3;
+
+  const handleChange = (item: { label: string; value: string }) => {
+    // If onTypeChange is provided, assume controlled component and notify parent
+    if (onTypeChange) {
+      onTypeChange(item.value);
+    } else {
+      // If not, update internal state
+      setInternalValue(item.value);
+    }
+  };
+
   return (
     <DropDownPicker
       items={items}
-      defaultValue={null}
-      placeholder={t('example:selectType')}
-      style={{ backgroundColor: isDark ? '#A6A4F0' : '#44427D' }}
+      defaultValue={defaultValue || internalValue}
+      placeholder={"Select Type"}
+      style={{
+        width: width,
+        backgroundColor: isDark ? '#A6A4F0' : '#44427D'
+      }}
       itemStyle={{ justifyContent: 'flex-start' }}
       dropDownStyle={{ backgroundColor: isDark ? '#A6A4F0' : '#44427D' }}
-      onChangeItem={item => onTypeChange(item.value)}
+      onChangeItem={handleChange}
     />
   );
 };
